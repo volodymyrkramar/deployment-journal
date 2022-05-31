fbe9d50a37d69d4b32cb14d53b46fc03fb5ad02d
vkramar <kramarjob@gmail.com>
2022-05-31 14:57:31 +0300

    initial commit

---
+++ src/main/java/com/dev/deploymentjournal/DeploymentJournalApplication.java
@@ -0,0 +1,13 @@
+package com.dev.deploymentjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentJournalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeploymentJournalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/deploymentjournal/DeploymentJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.deploymentjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentJournalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
